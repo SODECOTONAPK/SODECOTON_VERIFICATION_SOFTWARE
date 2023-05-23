1e2974bbbbdc8e37cf48d6d2b600ab90ea67329a
DOHICOU KOPSE <sodecotonapk@gmail.com>
2023-05-23 18:16:52 +0200

    Update2

--- src/main/java/com/test_1/test/HelloController.java
+++ src/main/java/com/test_1/test/HelloController.java
@@ -5,17 +5,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-//import javafx.scene.control.TabPane;
-//import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class HelloController {
